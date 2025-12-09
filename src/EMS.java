f383f33a3c44262688c039b70a5a25c36cb27f9e
daydre4mX <zerubbabelashenafi@gmail.com>
2025-12-09 14:57:22 -0500

    track .vscode to allow other developers to have a consistent experience

--- src/EMS.java
+++ src/EMS.java
@@ -1,6 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 public class EMS {
     public static void main(String[] args) throws Exception {
 
-        System.out.println("Hello, World!");
+        // attempt to load configuration file from root of classpath
+        try (InputStream props = EMS.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties databaseSettings = new Properties();
+
+            if (props == null) {
+                System.out.println("Could not find configuration file!");
+                return;
+            }
+
+            databaseSettings.load(props);
+
+            System.out.println();
+            System.out.println(databaseSettings.getProperty("db.url"));
+            System.out.println(databaseSettings.getProperty("db.user"));
+            System.out.println(databaseSettings.getProperty("db.password"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
     }
 }
