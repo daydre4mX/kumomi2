2f374a2a5e20aaa914d01b19062b549534eae349
Myles Miller <163357265+ozemoya@users.noreply.github.com>
2025-12-10 21:05:42 -0500

    Remove unnecessary comments and license template from Division.java

--- src/main/java/types/Division.java
+++ src/main/java/types/Division.java
@@ -1,13 +1,5 @@
 package main.java.types;
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- * Represents a division within the organization.
- * Corresponds to the division table in the database.
- */
 /**
  *
  * @author Myles Miller
