c9526a114fdbb8328ab92c5946fd9da240dd798c
daydre4mX <zerubbabelashenafi@gmail.com>
2025-12-11 16:43:43 -0500

    Formatting changes

--- src/test/Search_Tester.java
+++ src/test/Search_Tester.java
@@ -16,54 +16,59 @@ import main.java.types.Employee;
 
 public class Search_Tester {
 
-    Connection database;
+        Connection database;
 
-    public Search_Tester() {
-        database = DatabaseInteractor.getDatabaseConnection();
-    }
+        public Search_Tester() {
+                database = DatabaseInteractor.getDatabaseConnection();
+        }
 
-    /*Test for Search by Employee ID, Returns information if in DB */
-    @Test
-    void testSearchforID(){
-        ArrayList<Employee> result = UserSearch.searchForEmployeeID(database, 1001);
+        /* Test for Search by Employee ID, Returns information if in DB */
+        @Test
+        void testSearchforID() {
+                ArrayList<Employee> result = UserSearch.searchForEmployeeID(database, 1001);
 
-        assertNotNull(result);
-        assertEquals(1, result.size(), "If correrect, return a single employee.");
+                assertNotNull(result);
+                assertEquals(1, result.size(), "If correrect, return a single employee.");
 
-        Employee emp = result.get(0);
-        assertEquals(101,emp.getEmployeeID());
-        assertNotNull(emp.getfName(), "First name must be populated");
-        assertNotNull(emp.getlName(), "Last name must be populated");
-        assertNotNull(emp.getJobTitle(), "Job Title name must be populated");
-        assertNotNull(emp.getAddress(), "Address name must be populated");
-        assertNotNull(emp.getDivision(), "Division name must be populated");
-        assertNotNull(emp.getDemographics(), "Demo name must be populated");
-    }
+                Employee emp = result.get(0);
+                assertEquals(101, emp.getEmployeeID());
+                assertNotNull(emp.getfName(), "First name must be populated");
+                assertNotNull(emp.getlName(), "Last name must be populated");
+                assertNotNull(emp.getJobTitle(), "Job Title name must be populated");
+                assertNotNull(emp.getAddress(), "Address name must be populated");
+                assertNotNull(emp.getDivision(), "Division name must be populated");
+                assertNotNull(emp.getDemographics(), "Demo name must be populated");
+        }
 
-    /*Test for Search by Employee SSN, If SSN correct, returns matching SSN from employee that it found. */
-    @Test
-    void testSearchforSSN(){
-        ArrayList<Employee> result = UserSearch.searchForEmployeeSSN(database, "111-22-3333");
+        /*
+         * Test for Search by Employee SSN, If SSN correct, returns matching SSN from
+         * employee that it found.
+         */
+        @Test
+        void testSearchforSSN() {
+                ArrayList<Employee> result = UserSearch.searchForEmployeeSSN(database, "111-22-3333");
 
-        assertNotNull(result);
-        assertEquals(1, result.size(), "If correrect, return a single employee.");
+                assertNotNull(result);
+                assertEquals(1, result.size(), "If correrect, return a single employee.");
 
-        Employee emp = result.get(0);
-        assertEquals("111-22-3333", emp.getSSN());
-    }
+                Employee emp = result.get(0);
+                assertEquals("111-22-3333", emp.getSSN());
+        }
 
-    /*Test for Search by Employee DOB, If DOB correct, returns matching DOB from employee that it found. */
-    @Test 
-    void testSearchforDOB(){
-        LocalDate birthday = LocalDate.of(1990, 05, 05);
+        /*
+         * Test for Search by Employee DOB, If DOB correct, returns matching DOB from
+         * employee that it found.
+         */
+        @Test
+        void testSearchforDOB() {
+                LocalDate birthday = LocalDate.of(1990, 05, 05);
 
-        ArrayList<Employee> result = UserSearch.searchForEmployeeDOB(database, birthday);
+                ArrayList<Employee> result = UserSearch.searchForEmployeeDOB(database, birthday);
 
-        assertNotNull(result);
-        assertEquals(result.isEmpty(), "If correct, return a single employee.");
+                assertNotNull(result);
+                assertEquals(result.isEmpty(), "If correct, return a single employee.");
 
-        assertEquals(birthday, result.get(0).getDemographics().getBirthDate());
+                assertEquals(birthday, result.get(0).getDemographics().getBirthDate());
 
-        
-    }
+        }
 }
